0ace5c98c12e458113c137f0d48f3aa74873ae43
ysk1026 <>
2022-07-31 14:23:34 +0900

    3rd_lesson

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,6 +5,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar( title: Text('App')),
+        //그냥 중앙정렬은 body 다음 center로 감싸면되는데 그 외 위치로 정렬하려면 align + alignment 사용
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            // width height 꽉차게 주려면 double.infinity
+            width: 100, height: 100,
+            //바깥 여백: 마진
+            margin: EdgeInsets.all(20),
+            //마진 부분별로 주는 법: EdgeInsets.fromLTRB(left, top, right, bottom)
+            //margin: EdgeInsets.fromLTRB(30, 0, 0, 30) 이런식으로
+            // 내부 여백: 패딩
+            padding: EdgeInsets.all(20),
+            child: Text('패딩 적용'),
+            //나머지 박스 스타일들은 decoration안에 넣어야함
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(color: Colors.black)
+            ),
+          ),
+        )
+      )
+    );
+  }
+}
+
+
+class MyHomework extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +59,8 @@ class MyApp extends StatelessWidget {
             height: 100,
             child: Row(
                 // 밑에 alignment로 간격 조절
-                // row안에 children 만들어서 여러개 위젯 넣기기              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                // row안에 children 만들어서 여러개 위젯 넣기기
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Icon(Icons.phone),
                   Icon(Icons.message),
